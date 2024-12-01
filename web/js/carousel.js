a641aa8e76d057bb2021a7c618df960760bc4e69
DaBoss120 <teo.schneuwly@gmail.com>
2024-12-01 22:12:51 +0100

    Carousel Finished
    - [New] Carousel is now on the main page and functional.
    - [New] The carousel has arrows to change image manually.
    - [New] The carousel has little dots to go to a selected image manually.
    - [New] The images change every 5 seconds automatically.
    - [New] On hover animation for the carousel arrows and dots.
    - [Edit] The top nav bar elements on the far left and far right now have small margins on the sides depending on the viewing width.

---
+++ web/js/carousel.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let slideIndex = 1;
+let slideAuto = setInterval(() => {
+    plusSlides(1)
+}, 5000)
+showSlides(slideIndex)
+
+function plusSlides(nb) {
+    showSlides(slideIndex += nb);
+}
+
+function currentSlide(nb) {
+    showSlides(slideIndex = nb);
+}
+
+function showSlides(nb) {
+
+    let i;
+    let slides = document.querySelectorAll("#carousel-slides");
+    let dots = document.querySelectorAll("#dot");
+    if (nb > slides.length) { slideIndex = 1 };
+    if (nb < 1) { slideIndex = slides.length }
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    // Deactivate all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    // Show correct slide and dot
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    // Clear interval so when a dot or arrow is clicked the five seconds reset to prevent slide skip just after manual skip
+    clearInterval(slideAuto);
+    slideAuto = setInterval(() => {
+        plusSlides(1)
+    }, 5000)
+}
