448fec0b82a76a1804417e8cdb94ed240660e92e
DaBoss120 <teo.schneuwly@gmail.com>
2024-12-02 18:45:36 +0100

    Pages update
    - [new] Added base header content for the rest of the pages
    - [Edit] Made the website theme dark

--- web/js/script.js
+++ web/js/script.js
@@ -7,6 +7,8 @@ window.addEventListener('DOMContentLoaded', function () {
                     <li><a href="index.html">Accueil</a></li>
                     <li><a href="inscription.html">Inscription</a></li>
                     <li><a href="evenements.html">Événements</a></li>
+                    <li><a href="comite.html">Comité</a></li>
+
                     <li><a href="contact.html">Contact</a></li>
                 </ul>
     `
