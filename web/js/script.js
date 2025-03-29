816c6d507e854cd9f60de9082b6435706bcdf328
DaBoss120 <164732130+DaBoss120@users.noreply.github.com>
2025-03-29 19:47:46 +0100

    Update on gallery and more
    - [New] Added some text on the main page to present the dojo and Karate.
    - [New] Added the degree of the teachers under their name
    - [Edit] Changed the configuration of the contact to regroup all phone numbers under one block
    - [Edit] Removed some images from the gallery
    - [Edit] Removed (archived) the event page.
    - [Edit] Removed the default browser effect when clicking on links
    - [Edit] The mobile links do not change to blue when clicking on them and are now wider

--- web/js/script.js
+++ web/js/script.js
@@ -7,7 +7,6 @@ window.addEventListener('DOMContentLoaded', function () {
                 <ul>
                     <li><a href="index.html">Accueil</a></li>
                     <li><a href="inscription.html">Inscription</a></li>
-                    <li><a href="evenements.html">Événements</a></li>
                     <li><a href="comite.html">Comité</a></li>
                     <li><a href="contact.html">Contact</a></li>
                     <li><a href="gallerie.html">Gallerie</a></li>
